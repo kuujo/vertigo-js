a114c51781f7a380af09a523d7cf06c830b637cf
Jordan Halterman <jordan.halterman@gmail.com>
2013-10-14 22:47:56 -0700

    Add grouping tests.

---
+++ src/test/resources/grouping/grouping_test.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var test = require('testtools');
+var vertigo = require('vertigo');
+var grouping = require('vertigo/grouping');
+
+var grouping_tests = {
+  testRandomGrouping: function() {
+    var network = vertigo.createNetwork('test');
+    network.fromVerticle('test_verticle1', 'test_verticle1.js', {'foo': 'bar'}, 2).groupBy(new grouping.RandomGrouping())
+      .toVerticle('test_verticle2', 'test_verticle2.js', {'bar': 'baz'}).groupBy(new grouping.RandomGrouping());
+    test.testComplete();
+  },
+  testRoundGrouping: function() {
+    var network = vertigo.createNetwork('test');
+    network.fromVerticle('test_verticle1', 'test_verticle1.js', {'foo': 'bar'}, 2).groupBy(new grouping.RoundGrouping())
+      .toVerticle('test_verticle2', 'test_verticle2.js', {'bar': 'baz'}).groupBy(new grouping.RoundGrouping());
+    test.testComplete();
+  },
+  testFieldsGrouping: function() {
+    var network = vertigo.createNetwork('test');
+    network.fromVerticle('test_verticle1', 'test_verticle1.js', {'foo': 'bar'}, 2).groupBy(new grouping.FieldsGrouping('foo'))
+      .toVerticle('test_verticle2', 'test_verticle2.js', {'bar': 'baz'}).groupBy(new grouping.FieldsGrouping('bar'));
+    test.testComplete();
+  },
+  testAllGrouping: function() {
+    var network = vertigo.createNetwork('test');
+    network.fromVerticle('test_verticle1', 'test_verticle1.js', {'foo': 'bar'}, 2).groupBy(new grouping.AllGrouping())
+      .toVerticle('test_verticle2', 'test_verticle2.js', {'bar': 'baz'}).groupBy(new grouping.AllGrouping());
+    test.testComplete();
+  }
+}
+
+test.runTests(grouping_tests);
