a387f73c83b64c54debd67b4fb4e9568b033d8cd
Jordan Halterman <jordan.halterman@gmail.com>
2013-10-14 22:46:32 -0700

    Clean up grouping.js.

--- src/main/resources/vertigo/grouping.js
+++ src/main/resources/vertigo/grouping.js
@@ -1,17 +1,44 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 var grouping = {}
 
+/**
+ * A random grouping.
+ */
 grouping.RandomGrouping = function() {
   this.__jgrouping = new net.kuujo.vertigo.grouping.RandomGrouping();
 }
 
+/**
+ * A round-robin based grouping.
+ */
 grouping.RoundGrouping = function() {
   this.__jgrouping = new net.kuujo.vertigo.grouping.RoundGrouping();
 }
 
+/**
+ * A fields-based (consistent hashing) grouping.
+ */
 grouping.FieldsGrouping = function(field) {
   this.__jgrouping = new net.kuujo.vertigo.grouping.FieldsGrouping(field);
 }
 
+/**
+ * A "fanout" grouping.
+ */
 grouping.AllGrouping = function() {
   this.__jgrouping = new net.kuujo.vertigo.grouping.AllGrouping();
 }
