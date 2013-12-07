e12b309cf7304e37609ddda1de36a3db1ed92542
Jordan Halterman <jordan.halterman@gmail.com>
2013-12-07 03:37:27 -0800

    Add a simple word count example.

---
+++ examples/word_count/word_count_feeder.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var vertigo = require('vertigo');
+var console = require('vertx/console');
+
+var words = vertigo.feeder.config['words'];
+
+// Enable unlimited auto retries for timeouts.
+vertigo.feeder.autoRetry(true);
+vertigo.feeder.autoRetryAttempts(-1);
+
+// Register a feed handler that emits a random word each time it's called.
+vertigo.feeder.feedHandler(function(feeder) {
+  // Emit a random word.
+  var word = words[Math.floor(Math.random() * words.length)];
+  feeder.emit({word: word}, function(error) {
+    if (error) {
+      console.log(error.getMessage());
+    }
+  });
+});
